f4932b9528fb7b42b1cc0fbadba48b89c58bc4be
Isaac M <isaac.macedo@yahoo.com.br>
2022-06-24 20:39:05 -0300

    alterado script

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-	function animate(element) {
-	    let elementWidth = element.offsetWidth;
-	    let parentWidth = element.parentElement.offsetWidth;
+	function animate(elemento) {
+	    let larguraElemento = elemento.offsetWidth;//retorna a largura de um elemento no layout
+	    let larguraPai = elemento.parentElement.offsetWidth;
 	    let contador = 0;
 	    setInterval(() => {
-	        element.style.marginLeft = --contador + "px";
-	        if (elementWidth == -contador) {
-	            contador = parentWidth;
+	        elemento.style.marginLeft = --contador + "px";
+	        if (larguraElemento == -contador) {
+	            contador = larguraPai;
         	}
     }, 20);
 }
 
 let subtitulo = document.getElementById("sub");
 	animate(subtitulo);
+//https://pt.stackoverflow.com/questions/104181/remover-css-de-elemento
+// https://pt.stackoverflow.com/questions/14639/ignorar-css-em-determinado-trecho-da-p%C3%A1gina
+/*
+for (i = 0; i < subtitulo.length; i++){
+	document.getElementById('intro').classList.remove('principal');
+}
+*/
