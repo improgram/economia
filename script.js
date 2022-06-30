1cb21d9413fcd9ca26378a00932d91458858946f
Isaac M <isaac.macedo@yahoo.com.br>
2022-06-30 20:56:48 -0300

    criado novos ids

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let subtitulo = document.getElementById("sub");
-animate(subtitulo);
+let sub1 = document.getElementById("sub1");
+let sub2 = document.getElementById("sub2");
+	animate(sub1);
+	animate(sub2);
 
 	function animate(elemento) {
 	    let larguraElemento = elemento.offsetWidth; //retorna a largura de um elemento no layout
